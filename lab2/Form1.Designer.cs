06e2e0075d2a97afd8d410e44c3141b45f9b60a6
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2021-10-12 13:24:23 +0300

    Add files via upload

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+using System.Windows.Forms;
+
+namespace lab2
+{
+    partial class Параллелепипед
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openglControl1 = new SharpGL.OpenGLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openglControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openglControl1
+            // 
+            this.openglControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.openglControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openglControl1.DrawFPS = true;
+            this.openglControl1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.openglControl1.Location = new System.Drawing.Point(0, 0);
+            this.openglControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.openglControl1.Name = "openglControl1";
+            this.openglControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openglControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openglControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openglControl1.Size = new System.Drawing.Size(976, 450);
+            this.openglControl1.TabIndex = 0;
+            this.openglControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openglControl1.Load += new System.EventHandler(this.openglControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(901, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вправо";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(697, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Влево";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 120;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button3.Location = new System.Drawing.Point(800, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Вверх";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button4.Location = new System.Drawing.Point(800, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Вниз";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 120;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 120;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 120;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button5.Location = new System.Drawing.Point(800, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "стоп";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button8.Location = new System.Drawing.Point(901, 278);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Отдалить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button9.Location = new System.Drawing.Point(697, 278);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(84, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Приблизить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Параллелепипед
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chartreuse;
+            this.ClientSize = new System.Drawing.Size(976, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openglControl1);
+            this.Name = "Параллелепипед";
+            this.Text = "Параллелепипед";
+            ((System.ComponentModel.ISupportInitialize)(this.openglControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openglControl1;
+        private System.Windows.Forms.Button button1;
+        private Button button2;
+        private Timer timer1;
+        private Button button3;
+        private Button button4;
+        private Timer timer2;
+        private Timer timer3;
+        private Timer timer4;
+        private Button button5;
+        private Button button8;
+        private Button button9;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace lab2
+{
+
+    public partial class Параллелепипед : Form
+    {
+
+        float rotation = 0.0f;
+        float rotationx = 0;
+        float rotationy = 0;
+        float xcc = 1.5f;
+        float y = 0.0f;
+        float x = 0.0f;
+        float z = 0.0f;
+        float yc =- 1.0f;
+        float xc = 7;
+        bool right;
+        bool left;
+        //bool up;
+        //bool down;
+        public Параллелепипед()
+        {
+            InitializeComponent();
+        }
+        //public void OpenglControl1_Resized(object sender, EventArgs e)
+        //{
+        //    OpenGL gl = openglControl1.OpenGL;
+        //    gl.MatrixMode(OpenGL.GL_PROJECTION);
+        //    gl.LoadIdentity();
+        //    gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+        //    if (left == false)
+        //    {
+        //        gl.LookAt(7, 1, 0,  // (x,y,z)
+        //                 1, 0, 0,  // (направление)
+        //                 0, 1, 0); // (верх камеры)
+        //    }
+        //    else
+        //    {
+        //        gl.LookAt(xc, 1, 0,  // (x,y,z)
+        //              1, 0, 0,  // (направление)
+        //              0, 1, 0); // (верх камеры)
+        //    }
+
+        //    gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        //}
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            OpenGL gl = openglControl1.OpenGL;
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+            if (left == false)
+            {
+                gl.LookAt(7, 1, 0,  // (x,y,z)
+                         1, 0, 0,  // (направление)
+                         0, 1, 0); // (верх камеры)
+            }
+            else
+            {
+                gl.LookAt(xc, 1, 0,  // (x,y,z)
+                      1, 0, 0,  // (направление)
+                      0, 1, 0); // (верх камеры)
+            }
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.ClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+            //  Указываем оси вращения (x, y, z)
+            gl.Rotate(rotation, x, y, z);
+
+            gl.PolygonMode(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_FILL);
+            
+
+
+            gl.LineWidth(2);
+            gl.Color(0.0f, 0.0f, 0.0f);
+            gl.Begin(OpenGL.GL_POLYGON);
+
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 0f, 1f);
+
+            gl.Vertex(0f, 1.5f, 1f);
+            gl.Vertex(0f, 1.5f, -1f);
+
+            gl.End();
+            //Передняя грань
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(2f, 0f, 1f);
+
+            gl.Vertex(2f, 1.5f, 1f);
+            gl.Vertex(2f, 1.5f, -1f);
+
+            gl.End();
+            // Левая грань
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Vertex(2f, 0f, 1f);
+            gl.Vertex(0f, 0f, 1f);
+
+            gl.Vertex(0f, 1.5f, 1f);
+            gl.Vertex(2f, 1.5f, 1f);
+
+            gl.End();
+            //Правая грань
+
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(0f, 0f, -1f);
+
+            gl.Vertex(0f, 1.5f, -1f);
+            gl.Vertex(2f, 1.5f, -1f);
+
+            gl.End();
+            //нижняя часть
+
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 0f, 1f);
+
+            gl.Vertex(2f, 0f, 1f);
+            gl.Vertex(2f, 0f, -1f);
+            gl.End();
+            //верхняя часть
+
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Vertex(0f, 1.5f, -1f);
+            gl.Vertex(0f, 1.5f, 1f);
+
+            gl.Vertex(2f, 1.5f, 1f);
+            gl.Vertex(2f, 1.5f, -1f);
+            gl.End();
+
+            gl.LineWidth(3);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 0f, 1f);
+            gl.End();
+
+            gl.LineWidth(3);
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(0f, 1.5f, -1f);
+            gl.Vertex(0f, 1.5f, 1f);
+
+            gl.End();
+            gl.LineWidth(3);
+
+            gl.Begin(OpenGL.GL_LINES);
+ 
+            gl.Vertex(2f, 1.5f, -1f);
+            gl.Vertex(2f, 1.5f, 1f);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(2f, 0f, 1f);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+   
+            gl.Vertex(2f, 1.5f, 1f);
+            gl.Vertex(0f, 1.5f, 1f);
+
+            gl.Vertex(2f, 0f, 1f);
+            gl.Vertex(0f, 0f, 1f);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(2f, 1.5f, -1f);
+            gl.Vertex(0f, 1.5f, -1f);
+
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(0f, 0f, -1f);
+
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(0f, 0f, 1f);
+            gl.Vertex(0f, 1.5f, 1f);
+
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 1.5f, -1f);
+
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(2f, 0f, 1f);
+            gl.Vertex(2f, 1.5f, 1f);
+
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(2f, 1.5f, -1f);
+
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 1.5f, -1f);
+
+            gl.Vertex(0f, 0f, -1f);
+            gl.Vertex(0f, 1.5f, -1f);
+
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(2f, 1.5f, -1f);
+
+            gl.Vertex(2f, 0f, -1f);
+            gl.Vertex(2f, 1.5f, -1f);
+
+            gl.End();
+        }
+
+
+        private void openglControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            timer2.Enabled = true;
+            timer1.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer2.Start();
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            timer1.Stop();
+            timer2.Stop();
+            timer4.Stop();
+            timer3.Enabled = true;
+            timer3.Start();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Enabled = true;
+            timer4.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            OpenGL gl = new OpenGL();
+            rotation = -1.5f;
+            rotationx += 1.5f;
+            if (rotationx > 180)
+            {
+                rotationx = 0;
+            }
+            if (rotationx < 0)
+            {
+                rotationx = 180;
+            }
+
+          
+            y = -1.0f;
+            x = 0.0f;
+            z = 0.0f;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            OpenGL gl = new OpenGL();
+            rotation = 1.5f;
+            rotationx -= 1.5f;
+
+
+            if (rotationx > 180)
+            {
+                rotationx = 0;
+            }
+            if (rotationx < 0)
+            {
+                rotationx = 180;
+            }
+            y = -1.0f;
+            x = 0.0f;
+            z = 0.0f;
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            OpenGL gl = new OpenGL();
+            rotationy += 1.5f;
+            rotation = 1.5f;
+            y = 0.0f;
+            z = 1.0f;
+            x = 0.0f;
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            OpenGL gl = new OpenGL();
+            rotation = -1.5f;
+            rotationy -= 1.5f;
+            y = 0.0f;
+            z = 1.0f;
+            x = 0.0f;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenGL gl = openglControl1.OpenGL;
+            //gl.Enable(OpenGL.GL_CULL_FACE);
+            //gl.CullFace(OpenGL.GL_FRONT);
+            rotation = 0;
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            right = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            right = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            left = true;
+            xc += 1.5f;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            left = true;
+            xc -= 1.5f;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openglControl2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
